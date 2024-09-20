27889a513679f3725827f8f6ec9718c44f7b8cc9
Jackson Prowell <prowelljackson@gmail.com>
2024-09-20 11:36:22 -0700

    feat(drafting): animate a setting announcement while loading packs

--- app/routes/lobbies.$lobbyId/DraftStartingScreen.tsx
+++ app/routes/lobbies.$lobbyId/DraftStartingScreen.tsx
@@ -1,8 +1,12 @@
+import { useEffect, useState } from "react";
 import Heading from "~/components/Heading";
 import { useSettingStore } from "~/hooks/lobby/useSetting";
 
 const DraftStartingScreen = () => {
   const { setting } = useSettingStore();
+  const [fieldsToShow, setFieldsToShow] = useState<{ [key: string]: boolean }>(
+    {}
+  );
   const loadingDisplay = (
     <Heading>
       <div className="flex flex items-center gap-4">
@@ -12,10 +16,52 @@ const DraftStartingScreen = () => {
     </Heading>
   );
 
-  // const settingDisplay;
+  useEffect(() => {
+    if (!setting) return;
+    setFieldsToShow({
+      name: true,
+    });
+    const THESIS_TIMEOUT = 1000;
+    const MECHANIC_TIMEOUT = 3000;
+    setTimeout(() => {
+      setFieldsToShow((prev) => ({
+        ...prev,
+        thesis: true,
+      }));
+    }, THESIS_TIMEOUT);
+    setTimeout(() => {
+      setFieldsToShow((prev) => ({
+        ...prev,
+        mechanics: true,
+      }));
+    }, MECHANIC_TIMEOUT);
+  }, [setting]);
+
+  if (!setting)
+    return (
+      <div className="flex flex-col gap-8 flex-1 items-center justify-center">
+        {loadingDisplay}
+      </div>
+    );
   return (
-    <div className="flex flex-col gap-8 flex-1 items-center justify-center relative">
-      {setting ? <Heading>{setting.name}</Heading> : loadingDisplay}
+    <div className="flex flex-col gap-4 flex-1 justify-center items-center">
+      {fieldsToShow.name && (
+        <span className="animate-in fade-in zoom-in-90 duration-1000">
+          <Heading>{setting.name}</Heading>{" "}
+        </span>
+      )}
+      {fieldsToShow.thesis && (
+        <p className="animate-in fade-in zoom-in-90 duration-1000 max-w-[600px]">
+          {setting.thesis}
+        </p>
+      )}
+      {fieldsToShow.mechanics && (
+        <p className="animate-in fade-in zoom-in-90 duration-1000 max-w-[600px]">
+          <strong>{setting.newMechanic.name}</strong>
+          {": "}
+          {setting.newMechanic.rules_text}
+        </p>
+      )}
     </div>
   );
 };
