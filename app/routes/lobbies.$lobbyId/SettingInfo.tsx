9bd697382d75ed64b8e316b6218f03307dabd544
Jackson Prowell <prowelljackson@gmail.com>
2024-09-24 12:26:02 -0700

    style(setting display): hover dropdown on setting name

--- app/routes/lobbies.$lobbyId/SettingInfo.tsx
+++ app/routes/lobbies.$lobbyId/SettingInfo.tsx
@@ -6,9 +6,23 @@ const SettingInfo = () => {
 
   if (!setting) return null;
   return (
-    <div className="flex items-center gap-2">
+    <div className="flex items-center">
       <p className="font-bold text-xs uppercase">{setting.name}</p>
-      <InformationCircleIcon className="h-4 w-4 font-bold" />
+      <div className="dropdown dropdown-hover dropdown-bottom dropdown-end">
+        <div tabIndex={0} role="button" className="px-2">
+          <InformationCircleIcon className="h-4 w-4 font-bold" />
+          <div className="dropdown-content p-4 shadow bg-base-100 rounded-box w-96 z-10 prose">
+            <p className="text-sm">
+              <strong>Setting: </strong>
+              {setting.description}
+            </p>
+            <p className="text-sm">
+              <strong>{setting.newMechanic.name}: </strong>
+              {setting.newMechanic.rules_text}
+            </p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
